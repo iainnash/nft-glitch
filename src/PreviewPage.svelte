b80da93ba7d2ccb40c58fcfb4aa10376d30212c8
iain nash <me@iain.in>
2021-03-20 14:00:49 -0400

    mint

--- src/PreviewPage.svelte
+++ src/PreviewPage.svelte
@@ -1,5 +1,32 @@
 <script>
+  import MintButton from './MintButton.svelte';
+
   export let cid;
 </script>
 
-<iframe title="Site preview" src="https://ipfs.io/ipfs/{cid}">loading...</iframe>
+<div class="page">
+  <p>
+    Woah :o check out this masterpice.
+    <MintButton />
+  </p>
+
+  <iframe
+    width="80%"
+    height="80%"
+    class="frame"
+    title="Site preview"
+    src="https://ipfs.io/ipfs/{cid}">loading...</iframe
+  >
+</div>
+
+<style>
+  .frame {
+    border: 0;
+    margin: 10px;
+    box-shadow: 0 2px 4px -1px rgba(0,0,0,0.1),0 2px 4px -1px rgba(0,0,0,0.06);
+  }
+  .page {
+    display: flex;
+    flex-direction: row;
+  }
+</style>
