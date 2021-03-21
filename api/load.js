0a42d776462ab3317a23d3e2695798e1edd2dd74
iain nash <me@iain.in>
2021-03-21 05:19:57 -0400

    fix load

--- api/load.js
+++ api/load.js
@@ -5,15 +5,14 @@ module.exports = async (req, res) => {
           (() => {
             const pageParts = window.location.pathname.split('/');
             if (pageParts) {
-                const cloneStamp = document.createElement('div');
-                div.style.position = 'absolute';
-                div.style.top = 0;
+                const div = document.createElement('div');
+                div.style.padding = 10;
                 div.style.right = 0;
                 div.style.color = '#eee';
-                div.innerHTML = "<a target='_blank' href='https://nftglitch.vercel.app/editor/"+pageParts[pageParts.length-1]+"'>remix with nftglitch</a>"
+                div.innerHTML = "<a style='color:#555;font-size:11px' target='_blank' href='https://nftglitch.vercel.app/editor/"+pageParts[pageParts.length-1]+"'>remix with nftglitch</a>"
                 document.body.appendChild(div)
             }
-          });
+          })();
           `
       );
     };
